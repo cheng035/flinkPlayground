36f44504a15386d372f331d8ce13475584c9e556
Cheng Haohan <haohan.cheng@shopee.com>
2023-12-04 18:42:44 +0800

    add basic code

---
+++ src/main/java/ALertPrintOut.java
@@ -0,0 +1,15 @@
+import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.flink.walkthrough.common.entity.Alert;
+
+@PublicEvolving
+public class ALertPrintOut implements SinkFunction<Alert> {
+    private static final long serialVersionUID = 1L;
+
+    public ALertPrintOut() {
+    }
+
+    public void invoke(Alert value, Context context) {
+        System.out.println(value.toString());
+    }
+}
